5a6b21e5fe018216f76629b2a3114587f6435c9f
Alex <alexandrehgpaiva@outlook.com>
2025-03-17 11:53:24 -0300

    Creating Mock API

---
+++ MockAPIServer/Document.cs
@@ -0,0 +1,11 @@
+﻿namespace MockAPIServer
+{
+    public class Document
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public long PublishDate { get; set; }
+        public long ExpiryDate { get; set; }
+    }
+
+}
---
+++ MockAPIServer/Program.cs
@@ -0,0 +1,188 @@
+﻿using Newtonsoft.Json;
+using System.Reflection.Metadata;
+using System.Text.Json.Nodes;
+using WireMock;
+using WireMock.Matchers;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Util;
+
+namespace MockAPIServer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var server = WireMockServer.Start(5000);
+
+            SetupAuth(server);
+            SetupDocuments(server);
+
+            Console.WriteLine($"Mock server running at {server.Url}");
+            Console.WriteLine("Press any key to stop the server");
+
+            Console.ReadLine();
+
+            server.Stop();
+        }
+
+        private static void SetupAuth(WireMockServer server)
+        {
+            // /auth
+            server
+                .Given(Request.Create().WithPath("/auth").UsingPost())
+                .RespondWith(Response.Create()
+                    .WithCallback(request =>
+                    {
+                        var body = JsonConvert.DeserializeObject<dynamic>(request.Body);
+                        if ((body?.username) != "someone@someplace.net" || (body?.password) != "reind33r Fl0tilla")
+                        {
+                            return new ResponseMessage { StatusCode = 403 };
+                        }
+
+                        return new ResponseMessage
+                        {
+                            StatusCode = 200,
+                            BodyData = new BodyData { BodyAsJson = generateNewToken(), DetectedBodyType = WireMock.Types.BodyType.Json }
+                        };
+                    }));
+
+            // /auth/refresh
+            server
+                .Given(Request.Create().WithPath("/auth/refresh").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithCallback(request =>
+                    {
+                        if (!IsAuthorized(request, BearerToken))
+                        {
+                            return new ResponseMessage { StatusCode = 401 };
+                        }
+
+                        return new ResponseMessage
+                        {
+                            StatusCode = 200,
+                            BodyData = new BodyData { BodyAsJson = generateNewToken(), DetectedBodyType = WireMock.Types.BodyType.Json }
+                        };
+                    }));
+        }
+
+        private static void SetupDocuments(WireMockServer server)
+        {
+            var documents = new List<Document>{
+                new Document
+                {
+                    Id = "b3e1a6f4-8c2b-4d3a-9c3e-1f2b3a4d5e6f",
+                    Title = "A Sample Document",
+                    PublishDate = 1725148800,
+                    ExpiryDate = 1735603199
+                },
+                new Document
+                {
+                    Id = "b3e1a6f5-8c2b-4d3a-9c3e-1f2b3a4d5e6f",
+                    Title = "Another Sample Document",
+                    PublishDate = 1725148800,
+                    ExpiryDate = 1735603199
+                }
+            };
+
+            server
+                .Given(Request.Create().WithPath("/documents").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithCallback(request =>
+                    {
+                        if (!IsAuthorized(request, BearerToken))
+                        {
+                            return new ResponseMessage { StatusCode = 401 };
+                        }
+
+                        return new ResponseMessage
+                        {
+                            StatusCode = 200,
+                            BodyData = new BodyData { BodyAsJson = documents, DetectedBodyType = WireMock.Types.BodyType.Json }
+                        };
+                    }));
+
+            server
+                .Given(Request.Create().WithPath("/document/*").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithCallback(request =>
+                    {
+                        if (!IsAuthorized(request, BearerToken))
+                        {
+                            return new ResponseMessage { StatusCode = 401 };
+                        }
+
+                        var id = request.PathSegments[1];
+                        var document = documents.FirstOrDefault(d => d.Id == id);
+
+                        if (document == null)
+                        {
+                            return new ResponseMessage { StatusCode = 404 };
+                        }
+
+                        return new ResponseMessage
+                        {
+                            StatusCode = 200,
+                            BodyData = new BodyData { BodyAsJson = document, DetectedBodyType = WireMock.Types.BodyType.Json }
+                        };
+                    }));
+
+            server
+                .Given(Request.Create().WithPath("/document").UsingPost())
+                .RespondWith(Response.Create()
+                    .WithCallback(request =>
+                    {
+                        if (!IsAuthorized(request, BearerToken))
+                        {
+                            return new ResponseMessage { StatusCode = 401 };
+                        }
+
+                        if (string.IsNullOrEmpty(request.Body))
+                        {
+                            return new ResponseMessage { StatusCode = 400 };
+                        }
+
+                        var document = JsonConvert.DeserializeObject<Document>(request.Body);
+                        if (document == null || string.IsNullOrEmpty(document.Id) || string.IsNullOrEmpty(document.Title))
+                        {
+                            return new ResponseMessage { StatusCode = 400 };
+                        }
+
+                        var existingDocument = documents.FirstOrDefault(d => d.Id == document.Id);
+                        if (existingDocument != null)
+                        {
+                            documents.Remove(existingDocument);
+                            documents.Add(document);
+                            return new ResponseMessage { StatusCode = 200 };
+                        }
+
+                        documents.Add(document);
+                        return new ResponseMessage { StatusCode = 201 };
+                    }));
+        }
+
+        const string BearerToken = "a1b2c3d4-5e6f-7a8b-9c0d-1e2f3a4b5c6d";
+
+        private static object generateNewToken()
+        {
+            return new
+            {
+                bearerToken = BearerToken,
+                expiryDate = DateTimeOffset.UtcNow.AddMinutes(15).ToUnixTimeSeconds()
+            };
+        }
+
+        private static bool IsAuthorized(IRequestMessage request, string expectedToken)
+        {
+            if (request.Headers == null || !request.Headers.TryGetValue("Authorization", out var authorizationValues))
+            {
+                return false;
+            }
+
+            string? token = authorizationValues.FirstOrDefault();
+            return !string.IsNullOrEmpty(token) && token == expectedToken;
+        }
+    }
+
+}
